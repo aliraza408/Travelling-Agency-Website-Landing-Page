599c431e59b351a45885684243a5c1b28442b57c
aliraza408 <aliraz408@gmail.com>
2024-05-07 15:28:23 +0500

    Add javascript folder

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+//------------------------- Responsive navbar
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+
+navToggle.addEventListener("click", function () {
+  links.classList.toggle("show_nav");
+});
